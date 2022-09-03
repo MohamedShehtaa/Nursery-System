b5e6ce536c6350d82d79575602aca26553cb26ef
MohamedShehtaa <mmshehta92@gmail.com>
2022-09-03 05:23:26 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# Nursery-System
+
+Backend(Node js) web application for children’s nursery with login(admin and
+teacher), authentication, authorization, and MVC architecture.
+
+## Packages are used
+
+    -bcryptjs.
+    -body-parser.
+    -cors.
+    -dotenv.
+    -express.
+    -express-async-handler.
+    -express-validator.
+    -jsonwebtoken.
+    -mongoose.
+    -mongoose-sequence.
+    -morgan.
+    -multer.
+    -swagger-ui-express.
+
+## to run the app you should:
+
+    - as a prduction --> npm start
+    -as a development --> npm run dev
--- routes/teacherRoute.js
+++ routes/teacherRoute.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const { teacherPostValidation, teacherUpdateValidation } = require("../middleware/validation")
-const validationResult = require("./../middleware/validationResultMW");
-const uploadHandler = require("./../middleware/uploadimg")
+const express = require( "express" );
+const { teacherPostValidation, teacherUpdateValidation } = require( "../middleware/validation" )
+const validationResult = require( "./../middleware/validationResultMW" );
+const uploadHandler = require( "./../middleware/uploadimg" )
 
-const protect = require("../middleware/authMiddleware")
-const admin = require("../middleware/adminMiddleware")
-const own = require("../middleware/own")
+const protect = require( "../middleware/authMiddleware" )
+const admin = require( "../middleware/adminMiddleware" )
+const own = require( "../middleware/own" )
 const {
     getTeacher,
     updateTeacher,
@@ -15,28 +15,28 @@ const {
     changePassword,
     resetPassword,
     forgetPassword
-} = require("../controllers/teacherController");
+} = require( "../controllers/teacherController" );
 
-const { registerUser, loginUser } = require("../controllers/userController")
+const { registerUser, loginUser } = require( "../controllers/userController" )
 
 const router = express.Router();
 
-// router.use(["/teachers", "/teachers/:id", "/teachers/register"], protect, admin)
 
-router.route("/teachers")
-    .get(protect, admin, getTeachers)
-    .put(protect, teacherUpdateValidation, validationResult, updateTeacher)
 
-router.put("/teachers/profile", uploadHandler("teachers"), protect, own, updateTeacherImg)
+router.route( "/teachers" )
+    .get( protect, admin, getTeachers )
+    .put( protect, teacherUpdateValidation, validationResult, updateTeacher )
 
-router.route("/teachers/:id")
-    .get(protect, admin, getTeacher)
-    .delete(protect, admin, deleteTeacher)
+router.put( "/teachers/profile", uploadHandler( "teachers" ), protect, own, updateTeacherImg )
 
-router.route("/teachers/register").post(uploadHandler("teachers"), protect, own, teacherPostValidation, validationResult, registerUser)
-router.route("/teachers/login").post(loginUser)
-router.route("/teachers/changepassword").post(protect, own, changePassword)
-router.route("/teachers/forgetpassword").post(forgetPassword)
-router.route("/teachers/resetpassword").post(resetPassword)
+router.route( "/teachers/:id" )
+    .get( protect, admin, getTeacher )
+    .delete( protect, admin, deleteTeacher )
+
+router.route( "/teachers/register" ).post( uploadHandler( "teachers" ), protect, own, teacherPostValidation, validationResult, registerUser )
+router.route( "/teachers/login" ).post( loginUser )
+router.route( "/teachers/changepassword" ).post( protect, own, changePassword )
+router.route( "/teachers/forgetpassword" ).post( forgetPassword )
+router.route( "/teachers/resetpassword" ).post( resetPassword )
 
 module.exports = router;
